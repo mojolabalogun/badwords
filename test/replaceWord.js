7cd703902c121efae758cfb70a5d5d8b55f2430c
Mojola Balogun <31867604+mojolabalogun@users.noreply.github.com>
2023-07-25 01:48:26 -0700

    update for my use case

--- test/replaceWord.js
+++ test/replaceWord.js
@@ -3,10 +3,17 @@ var Filter = require('../lib/badwords.js'),
   filter = new Filter(),
   assert = require('better-assert');
 
-describe('filter', function(){
-	describe('replaceWord',function(){
-		it("Should replace a bad word with asterisks (******)",function(){
-			assert(filter.replaceWord("ash0le") == '******');
-		});
-	});
+describe('filter', function () {
+  describe('replaceWord', function () {
+    it('Should replace a bad word with asterisks (******)', function () {
+      assert(filter.replaceWord('ash0le') == '******');
+    });
+
+    it('Should keep the first character if provided as option (a*****)', function () {
+      var customFilter = new Filter({
+        keepFirstCharacter: true,
+      });
+      assert(customFilter.replaceWord('ash0le') == 'a*****');
+    });
+  });
 });
