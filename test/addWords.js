868d9e38ad1852009bbb00213a87c47969740f1c
Mojola Balogun <31867604+mojolabalogun@users.noreply.github.com>
2023-07-25 13:30:20 -0700

    add new filter method

--- test/addWords.js
+++ test/addWords.js
@@ -3,23 +3,23 @@ var Filter = require('../lib/badwords.js'),
   filter = new Filter(),
   assert = require('better-assert');
 
-describe('filter', function(){
-  describe('addWords',function(){
-    it('Should append words to the filter list.', function(){
+describe('filter', function () {
+  describe('addWords', function () {
+    it('Should append words to the filter list.', function () {
       filter.addWords('dog', 'go');
       assert(filter.clean('Go dog go') === '** *** **');
     });
 
     it('Should append words to the filter using an array', () => {
       let addWords = ['go', 'dog'];
-      filter = new Filter()
+      filter = new Filter();
       filter.addWords(...addWords);
       assert(filter.clean('Go dog go') === '** *** **');
     });
 
-    it('Should allow a list to be passed to the constructor', function() {
+    it('Should allow a list to be passed to the constructor', function () {
       filter = new Filter({
-        list: ['dog']
+        list: ['dog'],
       });
 
       assert(filter.clean('Go dog go') === 'Go *** go');
